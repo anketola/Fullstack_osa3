e6c3b13392f868286fc521f4cf06eaeb2bfb6405
Antti Ketola <antti@anttiketola.com>
2019-08-01 09:25:39 +0300

    Tehtävä 3.2 tehty

--- index.js
+++ index.js
@@ -27,7 +27,11 @@ let persons = [
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
-  
+
+app.get('/info', (req, res) => {
+  res.send(`<p>Phonebook has info for ${persons.length} people. <p><br /> ${new Date}`)
+})
+
 app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
