bc0a284325826a8fa06d0928d9ee222b896ee33c
Antti Ketola <antti@anttiketola.com>
2019-08-01 09:32:54 +0300

    Tehtävä 3.3 tehty

--- index.js
+++ index.js
@@ -32,6 +32,16 @@ app.get('/info', (req, res) => {
   res.send(`<p>Phonebook has info for ${persons.length} people. <p><br /> ${new Date}`)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
 app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
